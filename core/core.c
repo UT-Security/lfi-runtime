f998dbc54e6454737837a6b4c0da523ca12c1ba5
Taehyun Noh <taehyun@utexas.edu>
2026-03-06 17:29:33 -0600

    Use FF specific guardsize/stacksize

--- core/core.c
+++ core/core.c
@@ -124,6 +124,7 @@ lfi_new(struct LFIOptions opts, size_t nsandboxes)
     *engine = (struct LFIEngine) {
         .bm = bm,
         .opts = opts,
+        .guardsize = gb(2), // FF-specific
     };
 
     if (!engine->opts.no_init_sigaltstack)
