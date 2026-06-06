58ee6a1d073d86ca66fa7103894ee4448df18442
Anand Balaji <anandb@cs.utexas.edu>
2026-06-06 01:47:48 -0500

    verify only the compilation unit on jit code creation/modification

--- linux/jitcode.c
+++ linux/jitcode.c
@@ -151,8 +151,13 @@ jit_create_buf(struct LFILinuxProc *p, lfiptr dst, uint8_t *src, size_t length)
     uint8_t *alias_dst = jit_get_aliasptr(p, dst);
     memcpy(alias_dst, src, length);
 
-    // TODO: this will raise two mprotects NONE->READ-(verifier)->READ/EXEC
-    if (lfi_box_mprotect(p->box, base, len, LFI_PROT_READ | LFI_PROT_EXEC) < 0) {
+    // Verify only the jitcode compilation unit
+    if (!lfi_jitcode_verify(p->box, (uintptr_t)alias_dst, length)) {
+        lfi_box_munmap(p->box, base, len);
+        return -1;
+    }
+
+    if (lfi_box_mprotect_noverify(p->box, base, len, LFI_PROT_READ | LFI_PROT_EXEC) < 0) {
         return -1;
     }
 
@@ -224,8 +229,13 @@ proc_jit_modify(struct LFILinuxProc *p, lfiptr src, size_t value,
         memset(jit_addr + patch_offset - halt_pad_offset, 0xf4, halt_pad_offset);
     memcpy(jit_addr + patch_offset, &value, patch_len);
 
-    // TODO: this will raise two mprotects NONE->READ-(verifier)->READ/EXEC
-    if (lfi_box_mprotect(p->box, base, len, LFI_PROT_READ | LFI_PROT_EXEC) < 0) {
+    // Verify only the jitcode compilation unit
+    if (!lfi_jitcode_verify(p->box, (uintptr_t)jit_addr, size)) {
+        lfi_box_munmap(p->box, base, len);
+        return -1;
+    }
+
+    if (lfi_box_mprotect_noverify(p->box, base, len, LFI_PROT_READ | LFI_PROT_EXEC) < 0) {
         return -1;
     }
 
@@ -240,6 +250,7 @@ proc_jit_delete(struct LFILinuxProc *p, lfiptr addrp, size_t length)
         return -LINUX_EINVAL;
     if (!jit_codebufvalid(p, addrp, length))
       return -1;
+    memset(jit_get_aliasptr(p, addrp), 0xcc, length);
     return 0;
 }
 
